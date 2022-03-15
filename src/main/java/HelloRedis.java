c108d6921aa2373e7d3ea8b405023e09cc09f1eb
Bruno Dantas <brunoodantas7@gmail.com>
2022-03-14 23:01:22 -0300

    Basic crud actions with redis keys

--- src/main/java/HelloRedis.java
+++ src/main/java/HelloRedis.java
@@ -8,9 +8,23 @@ public class HelloRedis {
         Jedis jedis = new Jedis("127.0.0.1", 6379);
         jedis.auth("123");
 
+        System.out.println("======================================== ECHO =================================================");
         String result = jedis.echo("Hello Redis");
 
         System.out.println(result.toUpperCase());
+
+        System.out.println("======================================== SET =================================================");
+
+        result = jedis.set("last_logged_user", "Tony Stark");
+        System.out.println("RESULT -> "+result.toUpperCase());
+
+        System.out.println("========================================= GET ================================================");
+        System.out.println("VALUE CHANGED -> "+jedis.get("last_logged_user"));
+
+        System.out.println("========================================= DEL ================================================");
+        Long nrResult = jedis.del("last_logged_user");
+        System.out.println("RESULT OP DELETE (TOTAL OF DEL KEYS)-> "+nrResult);
+        System.out.println("=========================================================================================");
 
     }
 
